0d20f296af936c0af172770152f4afda3cc9fc3f
Alexander Baker <alex.baker685@gmail.com>
2019-01-24 23:50:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,15 +4,21 @@ class Artist
 
   attr_accessor :name
 
+  @@all = []
+  
   def initialize(name)
     @name = name
-    @songs = []
+    # @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def add_song(song)
     @songs << song
     song.artist = self
-
   end
 
   def add_song_by_name(name)
